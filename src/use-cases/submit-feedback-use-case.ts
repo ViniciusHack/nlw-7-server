ad6cf6c8d19bbc2cd0ff84379e7325c8efa94073
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-05-07 16:01:35 -0300

    chore: setup to deploy

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -15,11 +15,24 @@ export class SubmitFeedbackUseCase {
 
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
+
+    if(!type) {
+      throw new Error('Type is required.');
+    }
+
+    if(!comment) {
+      throw new Error('Comment is required.');
+    }
+
     await this.feedbackRepository.create({
       type,
       comment,
       screenshot
     })
+
+    if(screenshot && !screenshot.startsWith("data:image/png;base64")) {
+      throw new Error('Invalid screenshot format.');
+    }
 
     await this.mailAdapter.sendMail({
       subject: 'Novo Feedback',
@@ -27,6 +40,7 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`, 
         `<p>Tipo do feedback: ${type}</p>`, 
         `<p>Comentário: ${comment}</p>`,
+        screenshot && `<img src="${screenshot}"/>`,
         `</div>`].join('\n')
     })
   }
